f3fcb2ef9ea6450e69b751adb93352d1c0ff15cd
Qinka <qinka@live.com>
2017-03-15 09:39:47 +0800

    update

---
+++ auto/report1.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "report1"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "ctexart"
+    "ctexart10"
+    "listings"))
+ :latex)
+
